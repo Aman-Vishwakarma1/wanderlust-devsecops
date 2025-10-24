1e7f841cd5aff4c1c4478f605dd174893fbca197
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-10-24 20:04:20 +0530

    added docker files for frontend and backend and also added docker-compose file.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true, // listen on 0.0.0.0 (inside container)
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': 'http://backend_wanderlust:3000', // proxy API requests to backend container
     },
   },
 });
